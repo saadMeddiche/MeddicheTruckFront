88f5cfd36d0c914b97cbfa4d50cff5c11d72360d
saadMeddiche <saadmeddiche2004201@gmail.com>
2024-03-04 15:05:41 +0100

    Authentication Interceptor (of token)

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -1,5 +1,21 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpInterceptorFn, HttpRequest} from '@angular/common/http';
+import {AuthService} from "./auth.service";
+import {Injectable} from "@angular/core";
+import {Observable} from "rxjs";
 
-export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
-};
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private authService: AuthService) {}
+
+  intercept(request: HttpRequest<any>, handler: HttpHandler): Observable<HttpEvent<any>> {
+    const token = this.authService.getToken();
+    if (token) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+    }
+    return handler.handle(request);
+  }
+}
