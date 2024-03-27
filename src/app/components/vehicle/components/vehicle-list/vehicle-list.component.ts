28fb9909061002cfc628359204c6bb5ccd181efb
saadMeddiche <saadmeddiche2004201@gmail.com>
2024-03-27 11:34:05 +0000

    List vehicles

--- src/app/components/vehicle/components/vehicle-list/vehicle-list.component.ts
+++ src/app/components/vehicle/components/vehicle-list/vehicle-list.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import {BaseListComponent} from "../../../base/base-list/base-list.component";
+import {Piece} from "../../../piece/models/Piece";
+import {PieceService} from "../../../piece/services/piece.service";
+import {PopupService} from "../../../popup/services/popup.service";
+import {Router} from "@angular/router";
+import {VehicleService} from "../../services/vehicle.service";
+import {Vehicle} from "../../models/Vehicle";
 
 @Component({
   selector: 'app-vehicle-list',
   templateUrl: './vehicle-list.component.html',
   styleUrl: './vehicle-list.component.scss'
 })
-export class VehicleListComponent {
-
+export class VehicleListComponent extends BaseListComponent<Vehicle , "vehicles" , VehicleService>{
+  constructor(override itemService: VehicleService ,
+              override popup: PopupService,
+              override router: Router,
+  ) {
+    super(itemService ,popup , router);
+  }
 }
