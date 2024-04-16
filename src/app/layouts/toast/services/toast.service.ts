dc97f7149bc5bf9d2db70acd1c74fb75114f1afa
saadMeddiche <saadmeddiche2004201@gmail.com>
2024-04-16 12:18:17 +0100

    add remove animation to toast service

---
+++ src/app/layouts/toast/services/toast.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {Subject} from "rxjs";
+import {Toast} from "@app/layouts/toast/models/Toast";
+import {ToastType} from "@app/layouts/toast/enums/ToastType";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ToastService {
+
+  private toasts: Toast[] = [];
+  private toaster = new Subject<Toast[]>();
+  toaster$ = this.toaster.asObservable();
+
+  constructor() { }
+
+  pushToToaster(message :string , toastType :ToastType): void {
+    const toast = new Toast(message, toastType);
+    this.toasts.push(toast);
+    this.toaster.next(this.toasts);
+  }
+
+  cleanToaster(): void {
+    this.toasts = [];
+    this.toaster.next(this.toasts);
+  }
+
+  removeToast(uuid: string): void {
+    this.toasts = this.toasts.filter(toast => toast.uuid !== uuid);
+    this.toaster.next(this.toasts);
+  }
+}
