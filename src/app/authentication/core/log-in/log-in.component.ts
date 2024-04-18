a899a8765d157d46480f206f17edc30faa4eaf1b
saadMeddiche <saadmeddiche2004201@gmail.com>
2024-04-18 16:59:20 +0100

    Log in form validation

--- src/app/authentication/core/log-in/log-in.component.ts
+++ src/app/authentication/core/log-in/log-in.component.ts
@@ -4,25 +4,42 @@ import {AuthService} from "@app/authentication/services/authentication/auth.serv
 import {Router} from "@angular/router";
 import {ToastService} from "@app/layouts/toast/services/toast.service";
 import {Logo} from "@app/configurations/globalData";
-import {NavigationService} from "@app/services/navigation.service";
+import {ValidationService} from "@app/base/services/validation.service";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {noSpaceValidator, passwordValidator} from "@app/base/validation/costum-validators/costum.validators";
 
 @Component({
   selector: 'app-log-in',
   templateUrl: './log-in.component.html',
   styleUrl: './log-in.component.scss'
 })
-export class LogInComponent extends NavigationService{
+export class LogInComponent extends ValidationService{
 
   credentials :UsernameAndPassword = { username: '', password: '' };
+
+  protected readonly Logo = Logo;
 
   constructor(private authService: AuthService, override router: Router , private toastService :ToastService) {
     super(router);
   }
 
   logIn(): void {
+    this.credentials = this.form.value;
     this.authService.login(this.credentials).subscribe();
   }
 
+  override buildForm(): FormGroup {
+    return new FormGroup({
+      username: new FormControl('', [
+        Validators.required,
+        noSpaceValidator()
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        passwordValidator()
+      ])
+    });
+  }
 
-  protected readonly Logo = Logo;
+
 }
