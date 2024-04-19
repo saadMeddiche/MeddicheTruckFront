89a6d273e799923211b77bf1b5638adae354b7a8
saadMeddiche <saadmeddiche2004201@gmail.com>
2024-04-19 12:19:15 +0100

    Fix Vehicle list

--- src/app/components/vehicle/core/vehicle-list/vehicle-list.component.ts
+++ src/app/components/vehicle/core/vehicle-list/vehicle-list.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import {BaseListComponent} from "@app/base/core/base-list/base-list.component";
 import {Vehicle} from "@app/components/vehicle/models/vehicle";
 import {VehicleService} from "@app/components/vehicle/services/vehicle.service";
+import {ToastService} from "@app/layouts/toast/services/toast.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-vehicle-list',
@@ -10,6 +12,11 @@ import {VehicleService} from "@app/components/vehicle/services/vehicle.service";
 })
 export class VehicleListComponent extends BaseListComponent<Vehicle, VehicleService>
 {
-
-
+ constructor(
+     override itemService: VehicleService,
+     override toastService: ToastService,
+     override router: Router
+ ) {
+   super( itemService, toastService, router);
+ }
 }
