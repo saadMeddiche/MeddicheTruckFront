e738db926eb2a7d6dcb1cae81dc2110f43368fe1
saadMeddiche <saadmeddiche2004201@gmail.com>
2024-05-10 14:23:50 +0100

    Fix list base style

--- src/app/components/vehicle/data/VehicleList.ts
+++ src/app/components/vehicle/data/VehicleList.ts
@@ -58,12 +58,12 @@ public readonly extraRowButtons :RowButton<Vehicle>[] = [
       name: 'Transaction',
       class: 'inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-green-600 rounded-lg hover:bg-green-800 focus:ring-4 focus:ring-green-900',
     },
-    {
-      id: VehicleExtraRowButton.VEHICLE_TRANSACTIONS,
-      type: 'button',
-      name: 'Transactions',
-      class: 'inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-yellow-600 rounded-lg hover:bg-yellow-800 focus:ring-4 focus:ring-yellow-900',
-    }
+    // {
+    //   id: VehicleExtraRowButton.VEHICLE_TRANSACTIONS,
+    //   type: 'button',
+    //   name: 'Transactions',
+    //   class: 'inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-yellow-600 rounded-lg hover:bg-yellow-800 focus:ring-4 focus:ring-yellow-900',
+    // }
   ]
 }
 
