68732e37335994b1becc8d85017b1367fe7eddca
saadMeddiche <saadmeddiche2004201@gmail.com>
2024-05-20 19:05:29 +0100

    add env variables

--- src/app/configurations/api.ts
+++ src/app/configurations/api.ts
@@ -1,6 +1,7 @@
+import {environment} from "@env/environment";
 
-export const  BACKEND = 'http://localhost:8080';
+export const  BACKEND = environment.domain;
 
-export const BACKEND_API = `${BACKEND}/api/v1`;
+export const BACKEND_API = `${BACKEND}${environment.apiVersion}`;
 
 
--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
-  apiKey: 'devKey'
+  apiKey: 'devKey',
+  domain: 'http://localhost:8080',
+  apiVersion: '/api/v1',
 };
